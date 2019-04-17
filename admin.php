fdd1c7d1b69cf23af582a85c136896b93b899ff5
Nihao0 <45843275+Nihao0@users.noreply.github.com>
2019-04-17 10:18:09 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,10 +1,23 @@
 <?php
 require_once('authorize.php');
 ?>
-<html xmln<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Guitar Wars - High Scores Administration</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+  <h2>Guitar Wars - High Scores Administration</h2>
+  <p>Below is a list of all Guitar Wars high scores. Use this page to remove scores as needed.</p>
+  <hr />
+
+	
 <?php
-	require_once('appvars.php');
+		require_once('appvars.php');
 	require_once('connectvars.php');
 	
 	$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
@@ -17,26 +30,26 @@ require_once('authorize.php');
 	//Извлечение данных из массива рейтингов в цикле
 	//Форматирование данных записей в виде кода HTMl
 	echo '<table>';
+	echo '<tr><th>Name</th><th>Date</th><th>Score</th><th>Action</th></tr>';
 	while ($row = mysqli_fetch_array($data)) {
 		//Вывод данных рейтинга
-		echo '<tr class="scorerow"><td><strong>' . $row['name'] . '</strong></td>';
-		echo '<td>' . $row['score'] . '</td>';
-		echo '<td><a href="removescore.php?id=' . $row['id'] . '&amp;date=' . $row['date'] . 
-				'&amp;name=' . $row ['name'] . '&amp;score=' . $row['score'] .
-				'&amp;screenshot=' . $row['screenshot'] . '">Delete</a></td></tr>';
+	echo '<tr class="scorerow"><td><strong>' . $row['name'] . '</strong></td>';
+	echo '<td>' . $row['date'] . '</td>';
+	echo '<td>' . $row['score'] . '</td>';
+	echo '<td><a href="removescore.php?id=' . $row['id'] . '&amp;date=' . $row['date'] . 
+		'&amp;name=' . $row ['name'] . '&amp;score=' . $row['score'] .
+		'&amp;screenshot=' . $row['screenshot'] . '">Remove</a>';
 				
+	if ($row['approved'] == '0'){
+		echo ' / <a href="approvescore.php?id=' . $row['id'] . '&amp:date=' . $row['date'] . 
+			'&amp;name=' . $row ['name'] . '&amp;score=' . $row['score'] .
+			'&amp;screenshot=' . $row['screenshot'] . '">Approve </a>';
 	}
-
-if ($row['approved'] == '0'){
+	echo '</td></tr>';	
+}
+	echo '</table>';
 	
-	echo '/<a href="approvescore.php?id=' . $row['id']. '&amp:date=' . $row['date'] . 
-	'&amp;name=' . $row ['name'] . '&amp;score=' . $row['score'] .
-	'&amp;screenshot=' . $row['screenshot'] . '
-	
-	
-echo '</table>';
-	
-mysqli_close($dbc);
+	mysqli_close($dbc);
 ?>
-
+	</body>
 </html>
